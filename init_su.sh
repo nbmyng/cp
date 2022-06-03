fb0b3f87f3cdbc6394d13954d1a12919cd9331cc
nbmyng <nbmyng@gmail.com>
2022-06-03 23:47:55 +0900

    social login

--- init_su.sh
+++ init_su.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "------ create default admin user ------"
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@myapp.local', 'Passw0rd')" | python manage.py shell
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@myapp.local', 'adminadmin')" | python manage.py shell
